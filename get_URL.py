34571150d3a37e0b98f7b17320c12b63a0e98672
mkdiryuko <mtahtorhcai54891@gmail.com>
2023-08-12 00:26:40 +0900

    channel_id を指定すれば全動画を取得して、動画タイトルとURLを返すプログラムの実装

--- get_URL.py
+++ get_URL.py
@@ -22,4 +22,35 @@ def channel_name():
         channel_name.append(name)
     return channel_name
 
-print(channel_name())
+# チャンネルIDからチャンネルの全動画のプレイリストIDを取得
+# 引数に channel_id をとる。この時点で channel_id は未知（"湊アクア" のようなチャンネル名ではない）
+def getChannelPlaylistId(channel_id):
+    channel = youtube.channels().list(part='snippet,contentDetails', id=channel_id).execute()
+    item = channel['items'][0]
+    playlist_id = item['contentDetails']['relatedPlaylists']['uploads']
+    return playlist_id
+
+# プレイリストIDからプレイリストに含まれる動画のIDを取得
+def getVideoIds(playlist_id, page_token):
+    items_info = youtube.playlistItems().list(part='contentDetails', playlistId=playlist_id, maxResults=10, pageToken=page_token).execute()
+    video_ids = list(map(lambda item: item['contentDetails']['videoId'], items_info['items']))
+    if 'nextPageToken' in items_info:
+        video_ids.extend(getVideoIds(playlist_id, items_info['nextPageToken']))
+    return video_ids
+
+# 動画IDから動画の情報を取得
+def getVideos(video_ids):
+    videos = []
+    for index, video_id in enumerate(video_ids):
+        video_info = youtube.videos().list(part='snippet,statistics', id=video_id).execute()
+        videos.extend(video_info['items'])
+    return videos
+
+channel_id = 'UC9WJo5ZJVXMZiA5XV2jLx5Q'
+playlist_id = getChannelPlaylistId(channel_id)
+video_ids = getVideoIds(playlist_id, None)
+videos = getVideos(video_ids)
+
+for video in videos:
+    print(video['snippet']['title'], ',', 'https://youtube.com/watch?v=' + video['id'])
+    
